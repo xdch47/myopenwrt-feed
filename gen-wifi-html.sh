662e8773e410c5e6b390d51af2a73470a9226d15
Felix Neumärker <xdch47@posteo.de>
2022-11-08 12:48:15 +0100

    gen-wifi-html.sh: css

--- gen-wifi-html.sh
+++ gen-wifi-html.sh
@@ -12,6 +12,18 @@ html_escape() {
     echo "${var}"
 }
 
+if [[ -n "$(type -p base64)" ]] ; then
+  base64enc() {
+    base64 -w 0
+  }
+elif [[ -n "$(type p openssl)" ]] ; then
+  base64enc() {
+    openssl enc -A -base64
+  }
+else
+  echo "Missing base64 (coreutils-base64 or openssl) for encoding."
+fi
+
 html_header() {
     cat <<__EOF__
 <!DOCTYPE html>
@@ -24,17 +36,17 @@ html_header() {
 </head>
 <body bgcolor="#000">
   <div style='text-align:center;color:#fff;font-size:28px;font-weight:500;font-family: "LM Mono Proportional","Courier New",Courier,monospace;'>
-    <h1 style="textalign: center">WIFI ACCESS</h1>
+    <h1 style="margin-block-end: unset;">WIFI ACCESS</h1>
     <div style="display: flex; flex-wrap: wrap; justify-content: center">
 __EOF__
 }
 
 html_wifi() {
     cat <<__EOF__
-      <div style="margin: 4%; flex: 1 1 0;">
-        <p>SSID: <b>$(html_escape "${ssid}")</b></p>
+      <div style="margin: 4%; flex: 1 1 0; word-break: break-all;">
+        <p style="margin-block-start: unset";>SSID: <b>$(html_escape "${ssid}")</b></p>
         <p>PSK: <b>$(html_escape "${psk}")</b></p>
-        <img style="width: 80%" src="data:image/svg+xml;base64,$(echo "WIFI:T:WPA;S:${ssid};P:${psk};;" | qrencode --svg-path -t SVG -o - | base64 -w 0)"/>
+        <img style="width: min(80vw,40vh,20em)" src="data:image/svg+xml;base64,$(echo "WIFI:T:WPA;S:${ssid};P:${psk};;" | qrencode --svg-path -t SVG -o - | base64enc)"/>
       </div>
 __EOF__
 }
